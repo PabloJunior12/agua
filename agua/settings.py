b33edb58e89b469c8b124b3b5e7d88d38af798fb
PabloJunior12 <pablo_joseph01@hotmail.com>
2025-03-10 00:34:53 -0500

    add

--- agua/settings.py
+++ agua/settings.py
@@ -133,8 +133,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",  # Ejemplo para un frontend local
-    "https://mi-dominio.com",  # Dominio de producción
+    "https://agua.jsistemas.info/",  # Dominio de producciÃ³n
 ]
+
 
 
 # Static files (CSS, JavaScript, Images)
